639aa11be2292d572c34d42ce10388946a36179e
Rarynn <94757950+Rarynn@users.noreply.github.com>
2022-12-06 17:57:13 +0100

    PB mutex

--- peage.h
+++ peage.h
@@ -1,4 +1,6 @@
-
+#ifndef PEAGE_H
+#define PEAGE_H
+#include<stdbool.h>
 #include <stdio.h>
 #include <pthread.h>
 #include <stdlib.h>
@@ -7,14 +9,16 @@
 #include<semaphore.h>
 
 #include "voiture.h"
+#define NB_VOITURE 10 // nb de véhicules
 #define NB_PDP 5// nb de postes de péage
 //sem_t semaphore[NB_PDP]; // tableau de sémaphores
 
 typedef struct vehicules_pdp{
-   vehicule v;
+   int id;
    int pdp;
 }vehicule_pdp;
-
-
-void *peage(void *);
-
+void  Peage(int);
+void *fct_peage(void *);
+void voiture(int, int);
+void *fct_voiture(void *);
+#endif /* PEAGE_H */
